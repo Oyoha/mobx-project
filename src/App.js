51c12967aee689bfa25e871ec1ee2d8f28e58024
Васильев Алексей Леонидович <alexeyvasiliev98@gmail.com>
2025-01-24 15:41:37 +0700

    feat: сторы передаются через контекст

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Wrapper } from "./components/wrapper/Wrapper";
+import { RootStoreContext } from "./root-store-context";
+import RootStore from "./stores/root-store";
 
 function App() {
   return (
-    <div className="App">
-      <Wrapper />
-    </div>
+    <RootStoreContext.Provider value={new RootStore()}>
+      <div className="App">
+        <Wrapper />
+      </div>
+    </RootStoreContext.Provider>
   );
 }
 
--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,20 +1,21 @@
 import { observer } from "mobx-react-lite"
 import { Counter } from "../counter/Counter"
 import counterStore from "../../stores/counter-store";
-import postsStore from "../../stores/posts-store";
 import { useEffect } from "react";
+import { useStores } from "../../root-store-context";
 
 const counter1 = new counterStore();
 const counter2 = new counterStore();
 
 
 export const Wrapper = observer(() => {
-    const {getPostsAction, posts} = postsStore
+
+    const { post } = useStores()
 
     useEffect(() => {
-        getPostsAction()
+        post.getPostsAction()
     }, [])
-    console.log(posts)
+    console.log(post.posts)
     return (
         <>
             <Counter {...counter1} total={counter1.total}/>
